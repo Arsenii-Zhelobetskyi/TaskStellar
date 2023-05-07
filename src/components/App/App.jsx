d4fc4bae8040d0c219aac6b3040e4476c027126a
Arsenii-Zhelobetskyi <arsbrotherplay@gmail.com>
2023-05-07 23:13:45 +0300

    added Form

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,7 +1,20 @@
+import Form from "../Form/Form.jsx";
+import { useState } from "react";
+
 function App() {
+  const [externalText, setExternalText] = useState("");
+  const [storage, setStorage] = useState([]);
+  console.log(storage);
   return (
     <>
       <div>TaskStellar</div>
+      <Form
+        externalText={externalText}
+        storage={storage}
+        setStorage={setStorage}
+        setExternalText={setExternalText}
+      />
+      {/*<ul>{storage}</ul>*/}
     </>
   );
 }
